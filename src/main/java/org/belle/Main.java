2562561cd400c223d3d7eff8c5812fe830ad34c1
SamGarlick <samgarlick@hotmail.co.uk>
2024-01-01 15:57:56 +0000

    MVP

--- src/main/java/org/belle/Main.java
+++ src/main/java/org/belle/Main.java
@@ -6,7 +6,7 @@ import org.belle.server.Server;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Controller.connect("test");
+        Controller.connect("tm");
 
         new Server()
                 .start();
